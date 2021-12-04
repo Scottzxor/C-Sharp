308f7b0435cfc4f7bcfa8137993d1ede310f2691
Scottzxor <65826410+Scottzxor@users.noreply.github.com>
2021-12-04 08:37:19 -0600

    Add files via upload

---
+++ PigLatin/PigLatin/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿/* Scott Shaulis
+ * 04/12/2021
+ * Lesson 8: validating user input using a method; creating copies of strings until output becomes pig latin.
+ */
+
+namespace PigLatin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.englishWordTextBox = new System.Windows.Forms.TextBox();
+            this.middleLabel = new System.Windows.Forms.Label();
+            this.pigLatinWordLabel = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(14, 22);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(52, 13);
+            this.inputLabel.TabIndex = 0;
+            this.inputLabel.Text = "The word";
+            // 
+            // englishWordTextBox
+            // 
+            this.englishWordTextBox.Location = new System.Drawing.Point(77, 20);
+            this.englishWordTextBox.Name = "englishWordTextBox";
+            this.englishWordTextBox.Size = new System.Drawing.Size(95, 20);
+            this.englishWordTextBox.TabIndex = 1;
+            // 
+            // middleLabel
+            // 
+            this.middleLabel.AutoSize = true;
+            this.middleLabel.Location = new System.Drawing.Point(178, 23);
+            this.middleLabel.Name = "middleLabel";
+            this.middleLabel.Size = new System.Drawing.Size(62, 13);
+            this.middleLabel.TabIndex = 2;
+            this.middleLabel.Text = "in English is";
+            // 
+            // pigLatinWordLabel
+            // 
+            this.pigLatinWordLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pigLatinWordLabel.Location = new System.Drawing.Point(296, 20);
+            this.pigLatinWordLabel.Name = "pigLatinWordLabel";
+            this.pigLatinWordLabel.Size = new System.Drawing.Size(91, 20);
+            this.pigLatinWordLabel.TabIndex = 3;
+            this.pigLatinWordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Location = new System.Drawing.Point(393, 23);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(59, 13);
+            this.endLabel.TabIndex = 4;
+            this.endLabel.Text = "in Pig Latin";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(246, 13);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(31, 32);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "->";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 61);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.pigLatinWordLabel);
+            this.Controls.Add(this.middleLabel);
+            this.Controls.Add(this.englishWordTextBox);
+            this.Controls.Add(this.inputLabel);
+            this.Name = "Form1";
+            this.Text = "Pig Latin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.TextBox englishWordTextBox;
+        private System.Windows.Forms.Label middleLabel;
+        private System.Windows.Forms.Label pigLatinWordLabel;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ PigLatin/PigLatin/Form1.cs
@@ -0,0 +1,105 @@
+﻿/* Scott Shaulis
+ * 04/12/2021
+ * Lesson 8: validating user input using a method; creating copies of strings until output becomes pig latin.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLatin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            //changing text input to String variable for ease of use and legibility.
+            String userInput = englishWordTextBox.Text.Trim();
+
+            // declare variables
+            bool err = false;
+            char letter;
+
+            //checking 2 of the 3 conditions
+            if (userInput == "" || userInput.Length > 12)
+            {
+                err = true;
+                //clearing text and labels if error.
+                englishWordTextBox.Text = "";
+                pigLatinWordLabel.Text = "";
+            }
+
+            else { 
+
+                //creating loop to iterate through user's input.
+                for (int index = 0; index < userInput.Length; index++)
+                {
+                        //declaring variable at current indexed position in the loop for ease of use and legibility.
+                        letter = userInput[index];
+
+                        //declaring variable for sub-zero to convert to string so it can be added to end with Insert method. 
+                        char subZero = userInput[0];
+
+                        //converting char to string so it can be attached to end.
+                        string firstLetter = subZero.ToString();
+
+                        //My goodness, this feels very roundabout, and it feels like I'm doing something wrong.
+
+                        //declaring variable for user input's length for ease of use and readability.
+                        int userLength = userInput.Length;
+
+                        //checking third condition
+                        if (!Char.IsLetter(letter))
+                        {
+                            err = true;
+
+                        //clearing values 
+                        englishWordTextBox.Text = "";
+                        pigLatinWordLabel.Text = "";
+
+                        //adding break. Even if a non-char is found, the loop continues to iterate until the final value. If the final value is a char, the loop continues to the else statement and leaves the text and label boxes full.
+                        //Could probably be better with a while loop, but this is not in assignment directions, and this fixes it.
+                        break;
+
+                    }
+                        else
+                        {
+                        // Ironically, the inverse off-by-one error was possible here, without caution.
+                        // The length being variable causes problems indexing. Without removing the initial character first,
+                        // the quasi-StringArray is increased by 1, thereby changing the last position from, say, index[2] to index[3]. This means we cannot subtract 1 in order to compensate from starting from 0.
+
+                        string newString = userInput.Insert(userLength, firstLetter);
+
+                            string removedNewString = newString.Remove(0, 1);
+
+                            //length of the string has changed by -1, so no longer have to subtract 1 from userLength.
+                            string pigLatin = removedNewString.Insert(userLength, "ay");
+
+                            //print out the 2nd copy of the original string, after removal of first letter, inserting first letter in string form to the end, then inserting 'ay' onto the end of the string. *whew*: tough one for me.
+                            pigLatinWordLabel.Text = pigLatin;
+                        }
+                    }
+                }
+
+                //if the flag was tripped, show message box with general error message.
+                if (err == true)
+            {
+                MessageBox.Show("Invalid Input.");
+            }
+
+                //assignment really helped drill in that strings are immutable. Tough, but enjoyable, and hopefully sufficient and not the most inefficient.
+
+        }
+    }
+}
