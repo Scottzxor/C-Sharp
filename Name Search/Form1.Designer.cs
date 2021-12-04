eacdec82c5d772fd4ddbb55c43c48652fe57f744
Scottzxor <65826410+Scottzxor@users.noreply.github.com>
2021-12-04 08:40:52 -0600

    Add files via upload

---
+++ Name Search/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Name_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameDescriptionLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.popularityResultLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameDescriptionLabel
+            // 
+            this.nameDescriptionLabel.AutoSize = true;
+            this.nameDescriptionLabel.Location = new System.Drawing.Point(36, 29);
+            this.nameDescriptionLabel.Name = "nameDescriptionLabel";
+            this.nameDescriptionLabel.Size = new System.Drawing.Size(73, 13);
+            this.nameDescriptionLabel.TabIndex = 0;
+            this.nameDescriptionLabel.Text = "Enter a name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(115, 26);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(79, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(218, 18);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(153, 35);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Check Popularity";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // popularityResultLabel
+            // 
+            this.popularityResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.popularityResultLabel.Location = new System.Drawing.Point(393, 25);
+            this.popularityResultLabel.Name = "popularityResultLabel";
+            this.popularityResultLabel.Size = new System.Drawing.Size(260, 21);
+            this.popularityResultLabel.TabIndex = 3;
+            this.popularityResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(673, 18);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(66, 35);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 73);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.popularityResultLabel);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameDescriptionLabel);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameDescriptionLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label popularityResultLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Name Search/Form1.cs
@@ -0,0 +1,113 @@
+﻿/* Scott Shaulis
+ * 04/11/2021
+ * Copying data from txt file into array; sequential search to find a user-input value in an array.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Name_Search
+{
+
+
+    public partial class Form1 : Form
+    { 
+        
+        // create SequentialSearch method.
+        private int SequentialSearch(string[] sArray, string value)
+        {
+            //create sequential search variables
+            bool found = false;
+            int index = 0;
+            int position = -1;
+
+            // search array
+            while (!found && index < sArray.Length)
+            {
+                if (sArray[index] == value)
+                {
+                    //boolean flag breaks out of the while loop upon finding value
+                    found = true;
+
+                    // position switched to a number aside from -1, showing that something was found.
+                    position = index;
+                }
+                index++;
+            }
+
+            // return either -1 if not found or the int index of the position where value was found.
+            return position;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                popularityResultLabel.Text = "";
+                if (nameTextBox.Text == "")
+                {
+                    popularityResultLabel.Text = ("No input provided.");
+                }
+                else
+                {
+                    //declare index variable to iterate trhough file's index
+                    int index = 0;
+                    string userInput = nameTextBox.Text;
+
+                    //declare String array to accept the names from Names.txt
+                    String[] namesArr = new string[400];
+
+                    //declare streamreader object
+                    StreamReader inputFile;
+
+                    //open file using File.OpenText and assign the streamreader object 'inputFile' to the value of the open file.
+                    inputFile = File.OpenText("Names.txt");
+
+                    //read file's contents into the string using a while loop.
+                    while (index < namesArr.Length && !inputFile.EndOfStream)
+                    {
+                        namesArr[index] = inputFile.ReadLine();
+                        popularityResultLabel.Text = namesArr[index];
+                        index++;
+                    }
+
+                    //close the file
+                    inputFile.Close();
+
+                    // sequential search of array to see if input matches any element
+                    if(SequentialSearch(namesArr, userInput) != -1)
+                    {
+                        popularityResultLabel.Text = "This IS a popular name!";
+                    }
+                    else
+                    {
+                        popularityResultLabel.Text = "Sorry, this IS NOT a popular name!";
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
